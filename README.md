383f583860ed04e713145d5efcd842e267418f02
Meredith Meiyin Hu <meredithmhu@dhcp-vl2042-886.redrover.cornell.edu>
2024-10-05 15:57:17 -0400

    Reorganized script code to be a little cleaner and populated readme with project details

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# brh2024
-Project for Big Red Hacks 2024
-yay
+# smart-clothes-brh2024
+Project for [Big Red Hacks 2024](https://www.bigredhacks.com), Cornell University's annual hackathon. 
+
+**Project Overview**
+Conceptually an app that assists online clothing shoppers with selecting clothes that suit them by leveraging AI to suggest for them clothing available from the brand of their choice, if they first provide the AI with a descrption of themselves.
+
+Eventually, the app will allow them to virtually try on items on an accurate 3D representation of themselves. 
+
+## Relevant Technologies Used
+Python, [Meta LLaMa](https://www.llama.com), Dalai
+
+# Authors
+Designer - [Sriram Venkatassamy](https://www.linkedin.com/in/sriramvenkatassamy/)
+Developers - [Meredith Hu](https://github.com/meredithmhu), [Jake Donovan](https://github.com/jakedonovan025)
+
+# Project Description
+[Slide Deck]()
+
+# Future Updates 
+Contact Sriram who is developing this at [SUNY Oswego](https://ww1.oswego.edu). 
--- script.py
+++ script.py
@@ -1,37 +1,45 @@
-#List of user data that we'll call on to make the ChatGPT query
+#!/usr/bin/python3
+
+#imported libraries 
+
+"""
+script.py
+~Proof-of-Concept for the DressMe.ai~
+Script that already contains the profile information of the user, takes in a query from the user
+and asks an LLM for clothing recommendations for the user's profile and query. 
+Script then searches the database of a particular clothing brand's in-stock items for the recommended
+items and returns the found products. 
+""" 
+
+#Class that defines a user profile. User profile will be hard-coded for now
 class user:
     gender = 'female'
     race = 'Asian'
     age = '23'
     height = '5\'4\"'
-    size = '6'
+    dress_size = '6'
     skincolor = 'very pale'
     haircolor = 'black'
     hairlength = 'shoulder-length'
 
+'''
 meredith_user_profile = """I am an Asian female in her early 20's. 
 My height is 5'4 and my dress size is a size 6. 
 I am very pale-skinned and I have shoulder length black hair.
 """
+'''
 
+#user input, hard-coded for now 
 user_input = "wedding"
-zara_codes = []
-
-def main():
-    print("Hello World")
-
-main()
-
 #Obtain the most general query from the user through user input. This will be replaced by another form of user input through the developed UI later on.
-user_geninput = input('Enter a general query to start looking for clothes: ')
+user_geninput = input('What event or scenario would you like clothing recommendations for?: ')
 
 #Given a person's profile information and the general query, construct a detailed query to pass to the LLM.
-"""
-How a query can be structured?
-'What should I wear for a ' + user_geninput + '? I am a + user.race + ' ' + user.gender + ' aged ' + user.age + '. My height is ' + user.height + ' and I am a size ' + user_size + '. My skin is ' + user.skincolor + ' and my hair is ' + user.haircolor + ' and ' + user.hairlength + '.'
-"""
+query = 'What should I wear for a ' + user_geninput + '? I am a ' + user.race + " " + user.gender + ' aged ' + user.age + '. My height is ' + user.height + ' and I am a size ' + user.dress_size + '. My skin is ' + user.skincolor + ' and my hair is ' + user.haircolor + ' and ' + user.hairlength + '.'
 
-#Import the LLM and pass the query to  
+
+#Import the LLM (in this case, Meta LLaMa)
+"""
 from openai import OpenAI
 client = OpenAI()
 
@@ -48,6 +56,15 @@ completion = client.chat.completions.create(
 
 print(completion.choices[0].message)
 
+"""
+
 #take the output Chat gives, and use the output to search the database of choice (Zara? H&M? Amazon etc.) for matching items
 
 #get the image data and links and pass it to the fun visual output on the user interface.
+
+#main function for testing 
+def main():
+    print("Hello World")
+    print(query)
+
+main()
