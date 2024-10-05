55c66fb4e6962c5c8e27817d9d801e23ba3ba095
jakedonovan025 <jakedonovan025@gmail.com>
2024-10-04 22:48:20 -0400

    Update script.py
    bleh

--- script.py
+++ script.py
@@ -1,4 +1,12 @@
+meredith_user_profile = []
+user_input = "wedding"
+zara_codes = []
+
 def main():
     print("Hello World")
 
 main()
+#Obtain the most general query from the user (e.g. "wedding")
+#Given a person's profile info, ask ChatGPT the prompt user input, and give them the user's profile info. = "what would meredith look good in for a wedding"
+#take the output Chat gives, and use the output to search the database of choice (Zara? H&M? Amazon etc.) for matching items
+#get the image data and links and pass it to the fun visual output on the user interface.
